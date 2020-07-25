a5e6376cf2dfd8514b9458230491d2068de0d6a3
Sakshay Phanda <sakshayphanda03@gmail.com>
2020-07-25 14:05:32 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { AgmCoreModule } from '@agm/core/lib/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './containers/dashboard/dashboard.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MapComponent } from './components/map/map.component';
+import { SideBarComponent } from './containers/side-bar/side-bar.component';
+import { TableComponent } from './components/table/table.component';
+import { DynamicContentComponent } from './containers/dynamic-content/dynamic-content.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MapComponent,
+    SideBarComponent,
+    TableComponent,
+    DynamicContentComponent
   ],
   imports: [
     BrowserModule,
-    GoogleMapsModule
+    GoogleMapsModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/containers/side-bar/side-bar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { GlobalDataService } from '../../shared/services/global-data.service';
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrls: ['./side-bar.component.sass']
+})
+export class SideBarComponent implements OnInit {
+
+  regions = [];
+  constructor(
+    public globalDataService: GlobalDataService
+  ) { }
+
+  ngOnInit(): void {
+    this.globalDataService.regions.subscribe(
+      regions => {
+        console.log(regions);
+
+        this.regions = regions;
+      }
+    );
+  }
+
+}
